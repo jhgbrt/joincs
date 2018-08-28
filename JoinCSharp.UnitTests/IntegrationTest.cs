8b5714257700172450a96369e891d7d49424f010
Jeroen Haegebaert <Jeroen.haegebaert@ae.be>
2018-08-28 13:04:05 +0200

    avoid interference with git CRLF

--- JoinCSharp.UnitTests/IntegrationTest.cs
+++ JoinCSharp.UnitTests/IntegrationTest.cs
@@ -45,57 +45,55 @@ namespace JoinCSharp.UnitTests
 
             string result = sources.Join();
 
-            string expectedWithoutConditional = @"using Some.Using1;
-using Some.Using2;
-using Some.Using3;
-
-namespace Abc.Def
-{
-    class A
-    {
-    }
-
-    class B
-    {
-        public void y()
-        {
-            DomeSomething();
-        }
-    }
-}
-
-namespace Abc.Def.Ghi
-{
-    class C
-    {
-        public void x()
-        { // IDbConnection sql;
-            DomeSomething2();
-        }
-    }
-}
-
-namespace Xkcd.WhatIf
-{
-    class G
-    {
-    }
-}
-
-namespace Xyz.Vwu
-{
-    class E
-    {
-    }
-}
-
-class C
-{
-    public static dynamic x()
-    {
-        return null;
-    }
-}";
+            string expectedWithoutConditional =
+                "using Some.Using1;\r\n" +
+                "using Some.Using2;\r\n" +
+                "using Some.Using3;\r\n" +
+                "\r\n" +
+                "namespace Abc.Def\r\n" +
+                "{\r\n    class A\r\n" +
+                "    {\r\n" +
+                "    }\r\n" +
+                "\r\n" +
+                "    class B\r\n" +
+                "    {\r\n" +
+                "        public void y()\r\n" +
+                "        {\r\n" +
+                "            DomeSomething();\r\n" +
+                "        }\r\n" +
+                "    }\r\n" +
+                "}\r\n" +
+                "\r\nnamespace Abc.Def.Ghi\r\n" +
+                "{\r\n" +
+                "    class C\r\n" +
+                "    {\r\n" +
+                "        public void x()\r\n" +
+                "        { // IDbConnection sql;\r\n" +
+                "            DomeSomething2();\r\n" +
+                "        }\r\n" +
+                "    }\r\n" +
+                "}\r\n" +
+                "\r\n" +
+                "namespace Xkcd.WhatIf\r\n" +
+                "{\r\n" +
+                "    class G\r\n" +
+                "    {\r\n" +
+                "    }\r\n" +
+                "}\r\n" +
+                "\r\n" +
+                "namespace Xyz.Vwu\r\n" +
+                "{\r\n" +
+                "    class E\r\n" +
+                "    {\r\n" +
+                "    }\r\n" +
+                "}\r\n" +
+                "\r\nclass C\r\n" +
+                "{\r\n" +
+                "    public static dynamic x()\r\n" +
+                "    {\r\n" +
+                "        return null;\r\n" +
+                "    }\r\n" +
+                "}";
             //File.WriteAllText("result.txt", result);
             //File.WriteAllText("expected.txt", expectedWithoutConditional);
             //Process.Start(@"c:\Program Files (x86)\WinMerge\WinMergeU.exe", "result.txt expected.txt");
@@ -109,62 +107,63 @@ class C
             string result = sources.Join("CONDITIONAL");
 
             // TODO class comments are stripped
-            string expectedWithoutConditional = @"using Some.Using1;
-using Some.Using2;
-using Some.ConditionalUsing;
-using Some.Using3;
-
-namespace Abc.Def
-{
-    class A
-    {
-    }
-
-    class B
-    {
-        public void y()
-        {
-            DomeSomething();
-        }
-    }
-
-    class ConditionalClass
-    {
-    }
-}
-
-namespace Abc.Def.Ghi
-{
-    class C
-    {
-        public void x()
-        { // IDbConnection sql;
-            DomeSomething2();
-        }
-    }
-}
-
-namespace Xkcd.WhatIf
-{
-    class G
-    {
-    }
-}
-
-namespace Xyz.Vwu
-{
-    class E
-    {
-    }
-}
-
-class C
-{
-    public static dynamic x()
-    {
-        return null;
-    }
-}";
+            string expectedWithoutConditional =
+                "using Some.Using1;\r\n" +
+                "using Some.Using2;\r\n" +
+                "using Some.ConditionalUsing;\r\n" +
+                "using Some.Using3;\r\n" +
+                "\r\n" +
+                "namespace Abc.Def\r\n" +
+                "{\r\n" +
+                "    class A\r\n" +
+                "    {\r\n" +
+                "    }\r\n" +
+                "\r\n" +
+                "    class B\r\n" +
+                "    {\r\n" +
+                "        public void y()\r\n" +
+                "        {\r\n" +
+                "            DomeSomething();\r\n" +
+                "        }\r\n" +
+                "    }\r\n" +
+                "\r\n" +
+                "    class ConditionalClass\r\n" +
+                "    {\r\n" +
+                "    }\r\n" +
+                "}\r\n" +
+                "\r\n" +
+                "namespace Abc.Def.Ghi\r\n" +
+                "{\r\n" +
+                "    class C\r\n" +
+                "    {\r\n" +
+                "        public void x()\r\n" +
+                "        { // IDbConnection sql;\r\n" +
+                "            DomeSomething2();\r\n" +
+                "        }\r\n" +
+                "    }\r\n" +
+                "}\r\n" +
+                "\r\n" +
+                "namespace Xkcd.WhatIf\r\n" +
+                "{\r\n" +
+                "    class G\r\n" +
+                "    {\r\n" +
+                "    }\r\n" +
+                "}\r\n" +
+                "\r\n" +
+                "namespace Xyz.Vwu\r\n" +
+                "{\r\n" +
+                "    class E\r\n" +
+                "    {\r\n" +
+                "    }\r\n" +
+                "}\r\n" +
+                "\r\n" +
+                "class C\r\n" +
+                "{\r\n" +
+                "    public static dynamic x()\r\n" +
+                "    {\r\n" +
+                "        return null;\r\n" +
+                "    }\r\n" +
+                "}";
             //File.WriteAllText("result.txt", result);
             //File.WriteAllText("expected.txt", expectedWithoutConditional);
             //Process.Start(@"c:\Program Files (x86)\WinMerge\WinMergeU.exe", "result.txt expected.txt");
